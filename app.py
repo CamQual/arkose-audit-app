c6ba19fbaf4d15db432f54907658a573a0075537
CamQual <camille.g@arkose.com>
2026-04-21 18:59:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ if final_audio:
                 prompt = "Expert Arkose. Analyse l'audio. JSON obligatoire: nom_de_la_tache, liste_source, item, pole_concerne, prise_en_charge, criticite, red_flag(bool)."
                 
                 resp = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-2.0-flash',
                     contents=[f_up, prompt],
                     config=types.GenerateContentConfig(response_mime_type="application/json")
                 )
